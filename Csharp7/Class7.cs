321f29124c42072575df87eba00a0291a2dd2c45
unknown <c468aa777@gmail.com>
2022-12-19 17:46:42 +0300

    Я устал помогите

---
+++ Csharp7/Class7.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp7
+{
+
+    public class Prop
+    {
+        public Prop(string name1, string path1, bool folder1)
+        {
+            name = name1;
+            path = path1;
+            papka = folder1;
+        }
+        public string name;
+        public string path;
+        public bool papka;
+    }
+    public static class Papka
+    {
+        public static List<Prop> Get(string path)
+        {
+            List<Prop> propers = new List<Prop>();
+            foreach (string file in Directory.GetFiles(path))
+                propers.Add(new Prop(file, file, false));
+            foreach (string file in Directory.GetDirectories(path))
+                propers.Add(new Prop(file, file, true));
+            return propers;
+        }
+    }
+
+    public class Strelka
+    {
+        public int pos;
+        public int minpos;
+        public int maxpos;
+    }
+}
+
---
+++ Csharp7/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace Csharp7
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+            string path = " ";
+            List<Prop> props = new List<Prop>();
+            while (true)
+            {
+                Console.SetCursorPosition(0, 1);
+                if (path == " ")
+                {
+                    DriveInfo[] diski = DriveInfo.GetDrives();
+                    foreach (DriveInfo disk in diski)
+                    {
+                        double vsego = disk.TotalSize;
+                        double svobodno = disk.AvailableFreeSpace;
+                        string name = $"Имя диска:{disk.Name}: cвободно {svobodno} из {vsego}";
+                        props.Add(new Prop(name, disk.Name, true));
+                    }
+                }
+
+                else
+                {
+                    props.Clear();
+                    props = Papka.Get(path);
+                }
+
+                int stop = 0;
+                Strelka strelka = new Strelka();
+                strelka.pos = 1;
+                strelka.minpos = 0;
+                strelka.maxpos = 1;
+
+                foreach (Prop prop in props)
+                {
+                    strelka.maxpos++;
+                    Console.WriteLine("  " + prop.name);
+                }
+
+                while (stop != 1)
+                {
+
+                    Console.SetCursorPosition(0, strelka.pos);
+                    Console.Write("→");
+                    Console.SetCursorPosition(0, strelka.pos + 1);
+                    ConsoleKey key = Console.ReadKey().Key;
+                    switch (key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            Console.SetCursorPosition(0, strelka.pos);
+                            Console.Write(" ");
+
+                            strelka.pos--;
+
+                            if (strelka.pos == strelka.minpos)
+                            {
+                                strelka.pos++;
+                            }
+
+                            break;
+
+                        case ConsoleKey.DownArrow:
+                            Console.SetCursorPosition(0, strelka.pos);
+                            Console.Write(" ");
+
+                            strelka.pos++;
+
+                            if (strelka.pos == strelka.maxpos)
+                            {
+                                strelka.pos--;
+                            }
+
+                            break;
+                        case ConsoleKey.Enter:
+                            stop++;
+                            Prop prop = props[strelka.pos - 1];
+                            if (prop.papka)
+                            {
+                                path = prop.path;
+                                Console.Clear();
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
